f81ff864c088ec4dd1cafa10b531e75792bd1f4a
knr83 <avtochasty.com.ua@gmail.com>
2024-11-15 17:40:12 +0100

    Use config.json

--- app/config.py
+++ app/config.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 from dotenv import load_dotenv
@@ -5,12 +6,26 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
+# Load configuration from JSON file
+def load_config(config_file='config.json'):
+    if os.path.isfile(config_file):
+        with open('config.json') as config_file:
+            config = json.load(config_file)
+            print("Configuration loaded")
+            return config
+    else:
+        raise ValueError(f"Configuration not found: config.json")
 
 class Settings:
-    AZURE_CLIENT_ID = os.getenv("AZURE_CLIENT_ID")
-    AZURE_CLIENT_SECRET = os.getenv("AZURE_CLIENT_SECRET")
-    AZURE_TENANT_ID = os.getenv("AZURE_TENANT_ID")
-    REDIRECT_URI = os.getenv("REDIRECT_URI")
+    # AZURE_CLIENT_ID = os.getenv("AZURE_CLIENT_ID")
+    # AZURE_CLIENT_SECRET = os.getenv("AZURE_CLIENT_SECRET")
+    # AZURE_TENANT_ID = os.getenv("AZURE_TENANT_ID")
+    # REDIRECT_URI = os.getenv("REDIRECT_URI")
+    config = load_config()
+    AZURE_CLIENT_ID = config['azure_client_id']
+    AZURE_CLIENT_SECRET = config['azure_client_secret']
+    AZURE_TENANT_ID = config['azure_tenant_id']
+    REDIRECT_URI = config['redirect_uri']
 
     # Method to validate the presence of all required environment variables
     @classmethod
